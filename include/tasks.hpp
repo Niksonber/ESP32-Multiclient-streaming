ab860efc64f78271bf8f1fd201c111a4c7c13397
niksonber <nik.son18@hotmail.com>
2021-06-13 22:51:58 -0300

    Remove static atributes/pointer class arg in task

--- include/tasks.hpp
+++ include/tasks.hpp
@@ -8,7 +8,7 @@ public:
     
     /// Create task that runs periodicaly principal method @param name name of task
     /// @param priority (optional) default 2, @param core (optinal) default 1
-    static void run(const char * name, uint8_t priority = 2, uint8_t core = 1);
+    void run(const char * name, uint8_t priority = 2, uint8_t core = 1);
 
     /// Ends Task
     void stop();
@@ -21,23 +21,23 @@ public:
 
 protected:
 
-    /// Used to stop loop
-    static bool _running;
+    /// Used to stop task
+    bool _running =  true;
 
     /// Repetition period in ticks
-    static TickType_t _period;
+    TickType_t _period;
 
     /// Task priotity
-    static uint8_t _priority;
+    uint8_t _priority;
 
     /// Required stack size
-    static uint32_t _stackSize;
+    uint32_t _stackSize = 4 * 1024;
 
     /// Handler to task
-    static TaskHandle_t _handler;
+    TaskHandle_t _handler;
 
     /// internal loop of managers
-    static void _loop();
+    virtual void _loop();
 
     /// Repeat intenal loop periodicaly
     static void _run(void* args);
--- src/task.cpp
+++ src/task.cpp
@@ -1,20 +1,22 @@
 #include "tasks.hpp"
 
+
 void Task::run(const char * name, uint8_t priority, uint8_t core){
     _priority = priority;
-    xTaskCreatePinnedToCore(_run, name, _stackSize, NULL, _priority, &_handler, core);  
+    xTaskCreatePinnedToCore(_run, name, _stackSize, (void *) this, _priority, &_handler, core);  
 }
 
 void Task::_run(void* args){
+    Task * task = static_cast<Task *>(args);
     /// Last wakeup
     static TickType_t wakeUp = xTaskGetTickCount();
 
-    while ( _running ){
+    while ( task->_running ){
         /// Call inner loop of sons
-        _loop();
+        task->_loop();
 
         /// Ensure constant rate
         taskYIELD();
-        vTaskDelayUntil(&wakeUp, _period);
+        vTaskDelayUntil(&wakeUp, task->_period);
     }
 }
